db6e7f2ae34fcfd5e1048d07c95985d8bb544a0e
Ryan Bird <rlbird22@gmail.com>
2018-04-21 09:18:28 -0600

    Create initial classes for development

---
+++ ClueLessClient/ClueLessClient/Model/Game/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClueLessClient.Model.Game
+{
+    enum CardType
+    {
+        Room,
+        Location,
+        Weapon
+    }
+
+    class Card
+    {
+        private CardType cardType;
+        private String cardValue;
+    }
+}
---
+++ ClueLessClient/ClueLessClient/Model/Game/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClueLessClient.Model.Game
+{
+    class Game
+    {
+        private List<Player> players;  // Ryan changed from set to list
+        private Board board;
+        private Card[] caseFile;       // Casefile may be its own class, will leave as array for now
+    }
+}
