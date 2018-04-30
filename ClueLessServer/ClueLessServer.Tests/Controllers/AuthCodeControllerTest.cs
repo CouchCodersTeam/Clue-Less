0fc07fb72d1c04e3c7fcc61ec7aa9e440ac59359
Ryan Bird <rlbird22@gmail.com>
2018-04-29 19:14:26 -0600

    Create AuthCode API and unit tests using Dummy Data

---
+++ ClueLessServer/ClueLessServer.Tests/Controllers/AuthCodeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ClueLessServer.Controllers;
+using System.Web.Http.Results;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ClueLessServer.Tests.Controllers
+{
+    [TestClass]
+    public class AuthCodeControllerTest
+    {
+        [TestMethod]
+        public async Task TestGetAuthCode()
+        {
+            AuthCodeController controller = new AuthCodeController();
+
+            AuthCodeController.PlayerNameData testData = new AuthCodeController.PlayerNameData();
+
+            // test empty body
+            var result = controller.GetAuthCode(testData);
+            Assert.IsInstanceOfType(result, typeof(BadRequestErrorMessageResult));
+
+            testData.PlayerName = "Harry Potter";
+
+            result = controller.GetAuthCode(testData);
+
+            CancellationToken token = new CancellationToken();
+            
+            // Assert successful result
+            Assert.IsInstanceOfType(result, typeof(CreatedNegotiatedContentResult<Dictionary<string, object>>));
+            var response = (CreatedNegotiatedContentResult<Dictionary<string, object>>)result;
+            var responseData = response.Content;
+
+            // Test return data
+            object value = null;
+
+            Assert.IsTrue(responseData.ContainsKey("PlayerName"));
+            responseData.TryGetValue("PlayerName", out value);
+            Assert.AreEqual(testData.PlayerName, value);
+
+            Assert.IsTrue(responseData.ContainsKey("AuthCode"));
+            responseData.TryGetValue("AuthCode", out value);
+            Assert.AreEqual("123abc", value);
+        }
+    }
+}
---
+++ ClueLessServer/ClueLessServer/Controllers/AuthCodeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace ClueLessServer.Controllers
+{
+    public class AuthCodeController : ApiController
+    {
+        private const string PROVIDE_PLAYER_NAME = "Provide a {'PlayerName': [PlayerName]} in request body.";
+        
+        public class PlayerNameData
+        {
+            public string PlayerName { get; set; }
+        }
+
+        // POST: /auth_code
+        [Route("auth_code")]
+        [HttpPost]
+        public IHttpActionResult GetAuthCode([FromBody]PlayerNameData value)
+        {
+            if (value.PlayerName == null)
+            {
+                return BadRequest(PROVIDE_PLAYER_NAME);
+            }
+
+            // return dummy data
+            Dictionary<string, object> test = new Dictionary<string, object>();
+            test.Add("PlayerName", value.PlayerName);
+            test.Add("AuthCode", "123abc");
+
+            // return empty location. There is not a location to view this
+            // item
+            return Created("", test);
+        }
+    }
+}
