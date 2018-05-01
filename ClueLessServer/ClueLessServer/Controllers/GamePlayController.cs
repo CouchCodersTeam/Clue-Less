164a18ba70fe1935a90cbe42cde5037cd6064351
Ryan Bird <rlbird22@gmail.com>
2018-04-30 21:09:12 -0600

    Finish creating stencil APIs for character selection, gameplay, and game end

---
+++ ClueLessServer/ClueLessServer/Controllers/GamePlayController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClueLessServer.Controllers
+{
+    /*
+     * This class is in charge of setting up game components
+     * for example: Cards, etc
+     */
+    public class GamePlayController : ClueLessController
+    {
+        [Route("cards")]
+        [HttpGet]
+        // [Authorize]
+        public IHttpActionResult GetPlayerCards()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            // Return the calling player's cards
+            return NotFound();
+        }
+
+        [Route("charaters")]
+        [HttpGet]
+        public IHttpActionResult GetAvailableCharacters()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            return NotFound();
+        }
+
+        [Route("characters/{name}")]
+        [HttpPost]
+        public IHttpActionResult ChooseCharacter(string characterName)
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ ClueLessServer/ClueLessServer/Controllers/PlayerActionsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClueLessServer.Controllers
+{
+    /*
+     * This controller is in charge of API calls for taking
+     * a turn
+     */
+    public class PlayerActionsController : ClueLessController
+    {
+        [Route("state")]
+        [HttpGet]
+        public IHttpActionResult GetBoardState()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+
+            return NotFound();
+        }
+
+        [Route("command")]
+        [HttpGet]
+        public IHttpActionResult RequestCommand()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            // TODO: this will require waiting/async behavior
+            return NotFound();
+        }
+
+        [Route("move")]
+        [HttpPost]
+        public IHttpActionResult MovePlayer()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            return NotFound();
+        }
+
+        [Route("suggest")]
+        [HttpPost]
+        public IHttpActionResult MakeSuggestion()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            return NotFound();
+        }
+
+        [Route("disprove")]
+        [HttpPost]
+        public IHttpActionResult DisproveSuggestion()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            return NotFound();
+        }
+
+        [Route("accuse")]
+        [HttpPost]
+        public IHttpActionResult MakeAccusation()
+        {
+            AuthResult auth = authorizePlayerAndGame();
+            if (auth.result != null)
+                return auth.result;
+
+            return NotFound();
+        }
+
+        [Route("solution")]
+        [HttpGet]
+        public IHttpActionResult GetSolution()
+        {
+            AuthResult auth = authorizeGame();
+            if (auth.result != null)
+                return auth.result;
+
+            // Game must be over for this API to be called
+            return NotFound();
+        }
+    }
+}
