e94f20f5a69bc6e194893f27ebed87ebdd399aff
Ryan Bird <rlbird22@gmail.com>
2018-05-02 19:01:30 -0600

    Add static functions and class functions to Card
    * The static functions will be useful for deserializing card
      data sent from the server.

--- ClueLessClient/ClueLessClient/Model/Game/Card.cs
+++ ClueLessClient/ClueLessClient/Model/Game/Card.cs
@@ -48,12 +48,13 @@ namespace ClueLessClient.Model.Game
 
     public class Card
     {
+        // Instance variables
         private CardType cardType;
         private String cardValue;
 
         // Constructors
         // Usage:
-        // Card card = new Card(Study);
+        // Card card = new Card(Room.Study);
         public Card(Room room)
         {
             cardType = CardType.Room;
@@ -61,7 +62,7 @@ namespace ClueLessClient.Model.Game
         }
 
         // Usage:
-        // Card card = new Card(Mustard);
+        // Card card = new Card(Suspect.Mustard);
         public Card(Suspect suspect)
         {
             cardType = CardType.Suspect;
@@ -69,13 +70,103 @@ namespace ClueLessClient.Model.Game
         }
 
         // Usage:
-        // Card card = new Card(Rope);
+        // Card card = new Card(Weapon.Rope);
         public Card(Weapon weapon)
         {
             cardType = CardType.Weapon;
             cardValue = weapon.ToString();
         }
 
+        // Usage:
+        // Card card = new Card(...);
+        // CardType type = card.GetCardType();
+        public CardType GetCardType()
+        {
+            return cardType;
+        }
+
+        // Usage:
+        // Card card = new Card(...);
+        // String value = card.GetCardValue();
+        public String GetCardValue()
+        {
+            return cardValue;
+        }
+
+        // Usage:
+        // Weapon weapon = Card.GetWeaponFromString("Rope")
+        public static Weapon GetWeaponFromString(string value)
+        {
+            if (value.Equals("Candlestick"))
+                return Weapon.Candlestick;
+            else if (value.Equals("Revolver"))
+                return Weapon.Revolver;
+            else if (value.Equals("Knife"))
+                return Weapon.Knife;
+            else if (value.Equals("Pipe"))
+                return Weapon.Pipe;
+            else if (value.Equals("Rope"))
+                return Weapon.Rope;
+            else if (value.Equals("Wrench"))
+                return Weapon.Wrench;
+            else
+            {
+                throw new ArgumentException("Unknown Weapon: " + value);
+            }
+
+        }
+
+        // Usage:
+        // Suspect suspect = Card.GetSuspectFromString("Peacock")
+        public static Suspect GetSuspectFromString(string value)
+        {
+            if (value.Equals("Scarlet"))
+                return Suspect.Scarlet;
+            else if (value.Equals("Mustard"))
+                return Suspect.Mustard;
+            else if (value.Equals("White"))
+                return Suspect.White;
+            else if (value.Equals("Green"))
+                return Suspect.Green;
+            else if (value.Equals("Peacock"))
+                return Suspect.Peacock;
+            else if (value.Equals("Plum"))
+                return Suspect.Plum;
+            else
+            {
+                throw new ArgumentException("Unknown Suspect: " + value);
+            }
+
+        }
+
+        // Usage:
+        // Room room = Card.GetRoomFromString("Conservatory")
+        public static Room GetRoomFromString(string value)
+        {
+            if (value.Equals("Study"))
+                return Room.Study;
+            else if (value.Equals("Kitchen"))
+                return Room.Kitchen;
+            else if (value.Equals("Ballroom"))
+                return Room.Ballroom;
+            else if (value.Equals("Conservatory"))
+                return Room.Conservatory;
+            else if (value.Equals("Billiard"))
+                return Room.Billiard;
+            else if (value.Equals("Library"))
+                return Room.Library;
+            else if (value.Equals("Hall"))
+                return Room.Hall;
+            else if (value.Equals("Lounge"))
+                return Room.Lounge;
+            else if (value.Equals("Dining"))
+                return Room.Dining;
+            else
+            {
+                throw new ArgumentException("Unknown Suspect: " + value);
+            }
+
+        }
 
     }
     
