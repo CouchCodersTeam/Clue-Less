f01c8872080ecb43629dfa73ac7d43e2a1c3ee43
John Kvarta <jlkvarta@gmail.com>
2018-05-10 23:39:51 -0400

    created lobby.cs

---
+++ ClueLessClient/ClueLessClient/Model/Game/Lobby.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClueLessClient.Model.Game
+{
+    public class Lobby
+    {
+        private int MAXPLAYERS = 6;
+        private int MINPLAYERS = 3;
+        private List<Player> playersInLobby;
+
+        public Lobby()
+        {
+            playersInLobby = new List<Player>();
+
+        }
+
+    }
+}
